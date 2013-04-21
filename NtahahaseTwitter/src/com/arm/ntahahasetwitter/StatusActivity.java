69d99bf4d24eb2f7924a947b9ab9c2900a1bd96e
babame <babameadrian@yahoo.com>
2013-04-21 23:24:13 +0700

    Add Service Connect

--- NtahahaseTwitter/src/com/arm/ntahahasetwitter/StatusActivity.java
+++ NtahahaseTwitter/src/com/arm/ntahahasetwitter/StatusActivity.java
@@ -3,13 +3,19 @@
  */
 package com.arm.ntahahasetwitter;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.widget.EditText;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import com.arm.ntahahasetwitter.services.IStatusUpdateService;
+import com.arm.ntahahasetwitter.services.StatusServiceAdapter;
 
 /**
  * @author adrianbabame
@@ -18,15 +24,44 @@ import com.actionbarsherlock.view.MenuItem;
 public class StatusActivity extends SherlockActivity {
 	private static final String TAG = StatusActivity.class.getSimpleName();
 	private EditText edit_status;
+	
+	private NtahahaseApp mApp;
+	
+	private Intent mNtahahaseService;
+	private ServiceConnection mServiceConnection;
+	
+	private StatusServiceAdapter statusAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		mApp = (NtahahaseApp) getApplication();
+		mNtahahaseService = mApp.getServiceIntent();
+		registerServiceConnection();
 		setContentView(R.layout.status_activity);
 		edit_status = (EditText) findViewById(R.id.edit_status);
 		if (BuildConfig.DEBUG)
 			Log.i(TAG, "onCreate");
 	}
+	
+	
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		unbindService(mServiceConnection);
+	}
+
+
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		bindService(mNtahahaseService, mServiceConnection, BIND_AUTO_CREATE);
+		Log.i(TAG, "onResume");
+	}
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,8 +73,29 @@ public class StatusActivity extends SherlockActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		String status = edit_status.getText().toString();
+		statusAdapter.UpdateStatus(status);
 		if (BuildConfig.DEBUG)
 			Log.d(TAG, "status: " + status);
 		return true;
+	}
+	
+	private void registerServiceConnection() {
+		if (mApp.isServiceRunning())
+			mServiceConnection = new ServiceConnection() {
+
+				@Override
+				public void onServiceDisconnected(ComponentName name) {
+					Log.i(TAG, "called onServiceDisconnected()");
+				}
+
+				@Override
+				public void onServiceConnected(ComponentName name,
+						IBinder service) {
+					statusAdapter = new StatusServiceAdapter(
+							IStatusUpdateService.Stub.asInterface(service));
+					if (BuildConfig.DEBUG)
+						Log.i(TAG, "onServiceConnected()");
+				}
+			};
 	}
 }
