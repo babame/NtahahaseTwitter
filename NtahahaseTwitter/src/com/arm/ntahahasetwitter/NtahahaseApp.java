b33a8ccf93660632e27414eb3d90a274b5a30099
babame <babameadrian@yahoo.com>
2013-04-21 23:25:17 +0700

    Adding Register Service Function

--- NtahahaseTwitter/src/com/arm/ntahahasetwitter/NtahahaseApp.java
+++ NtahahaseTwitter/src/com/arm/ntahahasetwitter/NtahahaseApp.java
@@ -1,13 +1,18 @@
 package com.arm.ntahahasetwitter;
 
 import android.app.Application;
+import android.content.Intent;
 import android.util.Log;
+
+import com.arm.ntahahasetwitter.services.NtahahaseService;
 
 public class NtahahaseApp extends Application {
 	private static final String TAG = NtahahaseApp.class.getSimpleName();
 	
 	private boolean serviceRunning;
-
+	
+	public Intent mNtahahaseService;
+	
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -32,5 +37,19 @@ public class NtahahaseApp extends Application {
 
 	public void setServiceRunning(boolean serviceRunning) {
 		this.serviceRunning = serviceRunning;
+	}
+	
+	public Intent getServiceIntent() {
+		return mNtahahaseService;
+	}
+	
+	public void registerNtahahaseService() {
+		mNtahahaseService = new Intent(getApplicationContext(),
+				NtahahaseService.class);
+
+		mNtahahaseService
+				.setAction("com.arm.ntahahasetwitter.NTAHAHASESERVICE");
+		if (!isServiceRunning())
+			startService(mNtahahaseService);
 	}
 }
