89f821ebd8e5a6ab34f8be9c38845a57c53010c7
babame <babameadrian@yahoo.com>
2013-04-22 21:27:43 +0700

    Revert back

--- NtahahaseTwitter/src/com/arm/ntahahasetwitter/NtahahaseApp.java
+++ NtahahaseTwitter/src/com/arm/ntahahasetwitter/NtahahaseApp.java
@@ -1,18 +1,13 @@
 package com.arm.ntahahasetwitter;
 
 import android.app.Application;
-import android.content.Intent;
 import android.util.Log;
-
-import com.arm.ntahahasetwitter.services.NtahahaseService;
 
 public class NtahahaseApp extends Application {
 	private static final String TAG = NtahahaseApp.class.getSimpleName();
-	
+
 	private boolean serviceRunning;
-	
-	public Intent mNtahahaseService;
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -30,26 +25,12 @@ public class NtahahaseApp extends Application {
 		super.onTerminate();
 		Log.i(TAG, "onTerminate");
 	}
-	
+
 	public boolean isServiceRunning() {
 		return serviceRunning;
 	}
 
 	public void setServiceRunning(boolean serviceRunning) {
 		this.serviceRunning = serviceRunning;
-	}
-	
-	public Intent getServiceIntent() {
-		return mNtahahaseService;
-	}
-	
-	public void registerNtahahaseService() {
-		mNtahahaseService = new Intent(getApplicationContext(),
-				NtahahaseService.class);
-
-		mNtahahaseService
-				.setAction("com.arm.ntahahasetwitter.NTAHAHASESERVICE");
-		if (!isServiceRunning())
-			startService(mNtahahaseService);
 	}
 }
